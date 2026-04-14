04e8bd23789c8651efdc7c85fd1b29ee9be44087
Ivan Khanas <xeno@altlinux.org>
2026-04-15 00:50:03 +0300

    Sonar: handle interrupted animation thread correctly

--- tg-bot-service/src/main/kotlin/com/xeno/subpilot/tgbot/util/AIResponseWaitingIndicator.kt
+++ tg-bot-service/src/main/kotlin/com/xeno/subpilot/tgbot/util/AIResponseWaitingIndicator.kt
@@ -49,6 +49,7 @@ class AIResponseWaitingIndicator(
                     )
                 }
             } catch (_: InterruptedException) {
+                Thread.currentThread().interrupt()
             }
         }
 
