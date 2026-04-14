17b107c9a370d800727462b378a2c75b1a65e3b9
Ivan Khanas <xeno@altlinux.org>
2026-04-14 23:05:53 +0300

    Refactor: remove chat-service local model-preference persistence layer

--- chat-service/src/main/kotlin/com/xeno/subpilot/chat/repository/ChatModelPreferenceJpaRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.xeno.subpilot.chat.repository
-
-import com.xeno.subpilot.chat.entity.ChatModelPreference
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ChatModelPreferenceJpaRepository : JpaRepository<ChatModelPreference, Long>
