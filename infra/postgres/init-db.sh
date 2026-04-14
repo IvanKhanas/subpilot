e67c1274e5b15614b1fa10c29d741e17b5a2134c
Ivan Khanas <xeno@altlinux.org>
2026-04-14 23:09:51 +0300

    Fix: align env and docker-compose with subscription topology

--- infra/postgres/init-db.sh
+++ infra/postgres/init-db.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    SELECT 'CREATE DATABASE chat' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chat')\gexec
+psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
     SELECT 'CREATE DATABASE subscription' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'subscription')\gexec
     SELECT 'CREATE DATABASE payment' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'payment')\gexec
+
+    SELECT 'CREATE USER subscription WITH PASSWORD ''${SUBSCRIPTION_DB_PASSWORD}''' WHERE NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'subscription')\gexec
+    GRANT ALL PRIVILEGES ON DATABASE subscription TO subscription;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username postgres --dbname subscription <<-EOSQL
+    GRANT ALL ON SCHEMA public TO subscription;
 EOSQL
