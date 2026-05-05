7fbf4976501e20ac1db3e2c8318c6f841ee67de5
Ivan Khanas <xeno@altlinux.org>
2026-05-05 17:09:10 +0200

    Chore: update license years from 2024 to 2026

--- tg-bot-service/src/main/kotlin/com/xeno/subpilot/tgbot/service/SubscriptionActivatedConsumer.kt
+++ tg-bot-service/src/main/kotlin/com/xeno/subpilot/tgbot/service/SubscriptionActivatedConsumer.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Ivan Khanas
+ * Copyright 2026 Ivan Khanas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
